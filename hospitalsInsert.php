4719eb770c37d902edc535d74ceb4e2d9655c0d3
Emir Balkan <emir_balkan@hotmail.com>
2022-05-27 20:08:41 +0300

    departments and hospitals added

--- hospitalsInsert.php
+++ hospitalsInsert.php
@@ -6,7 +6,9 @@ if(!empty($_POST['hname']))
     $hname = $_POST['hname'];
     $personalsize = $_POST["personalsize"];
     $sql_statement = "INSERT INTO hospitals(Hid ,Hname, PersonalSize) VALUES('$hid','$hname', $personalsize)";
-   $result = mysqli_query($db, $sql_statement);
-    echo "Your result is : " . $result;
+    $result = mysqli_query($db, $sql_statement);
+    echo "The hospital " . $hname . " has been added succesfully.";
 }
+echo "</ul>";
 ?>
+<a href="admin.php">Go back to main page</a>
